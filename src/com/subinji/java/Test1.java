86304f05ec66009a61f3518dd021c2bc2e38a4b1
Ji-subin-97 <qls1178@naver.com>
2021-07-12 10:40:47 +0900

    update test2

--- src/com/subinji/java/Test1.java
+++ src/com/subinji/java/Test1.java
@@ -6,16 +6,19 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("ㅇㅇㅇㅇ");
+		System.out.println("숫자를 입력해주세요.");
 		
 		
-		int input = 5;
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
 		int output;
 		
 		output = input/4*10 + input%4;
 		
 		System.out.println(input);
 		System.out.println(output);
+		
+		//새로운 코드 추가
 	}
 
 }
