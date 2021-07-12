c4c2b9b3b365f7a0c6f654b7192605c520a2a27e
Ji-subin-97 <qls1178@naver.com>
2021-07-12 10:33:16 +0900

    update test
    !!

--- src/com/subinji/java/Test1.java
+++ src/com/subinji/java/Test1.java
@@ -1,10 +1,21 @@
 package com.subinji.java;
 
+import java.util.Scanner;
+
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("ㅇㅇㅇㅇ");
+		
+		
+		int input = 5;
+		int output;
+		
+		output = input/4*10 + input%4;
+		
+		System.out.println(input);
+		System.out.println(output);
 	}
 
 }
